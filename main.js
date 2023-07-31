10fb026ff2559bb8b343ab1486d99f751d14760f
salem <muhammedsalem992@gmail.com>
2023-07-31 10:45:56 -0700

    salem

---
+++ main.js
@@ -0,0 +1,10 @@
+var menudiv = document.querySelector(".menu");
+var mobilelinks = document.querySelector(".mobile_header-link")
+
+
+
+function dosomething() {
+    menudiv.classList.toggle("showburger")
+    mobilelinks.classList.toggle("show-exdiv2")
+
+}
